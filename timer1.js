251842d2e39f74519f0c86f83a75ea022cbe4198
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-01 19:32:21 +0000

    some refactoring of timer code

--- timer1.js
+++ timer1.js
@@ -1,8 +1,8 @@
 for (let i = 2; i < process.argv.length; i++) {
-  if ((process.argv.sort())[i] >= 1) {
+  if ((process.argv.sort())[i] > 0) {
     setTimeout(() => {
-      console.log((process.argv.sort())[i]);
-      process.stdout.write('\x07');
+      console.log(`Beep.. Seconds:${(process.argv.sort())[i]} \x07`);
+      //process.stdout.write('\x07');
     }, (((process.argv.sort())[i]) * 1000));
   }
 }
